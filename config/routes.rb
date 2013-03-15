a6c91bee51807c486dd4012f16214e5231dac63e
oded-soffrin <oded@moment.me>
2013-03-15 13:03:52 +0200

    finish cucumber chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 SampleApp::Application.routes.draw do
 
-  resources :users
+  #session resources
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  #static pages
   root :to => "static_pages#home"
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
+  #users resources
+  resources :users
   get '/signup', to: 'users#new'
+
 end
